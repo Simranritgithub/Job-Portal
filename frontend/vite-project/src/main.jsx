22059f61d5ed3ba729f812e1f7ae1bef4cfa4223
Simranritgithub <simranrout8796@gmail.com>
2025-08-13 12:59:58 +0530

    Update main.jsx

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import appRouter from './App'
 import { Provider } from 'react-redux'
 import { Toaster } from 'sonner'
 import store from './Components/redux/store'
 const container=document.getElementById('root')
 ReactDOM.createRoot(container).render(
-  <StrictMode>
+   <StrictMode>
     <Provider store={store}>
-    <App /></Provider>
-    <Toaster/>
-  </StrictMode>,
-)
+      <RouterProvider router={appRouter} />
+      <Toaster />
+    </Provider>
+  </StrictMode>
+);
